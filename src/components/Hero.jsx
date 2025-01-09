20259af62fea906df42f54e172e6dbe803c0cc92
Ritvik Bharti <bhartiritvik1000@gmail.com>
2025-01-09 17:39:07 +0530

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I am a passioanate Software Developer <br className='sm:block hidden' />
-            and a Android App Developer.
+            and an Android App Developer.
           </p>
         </div>
       </div>
